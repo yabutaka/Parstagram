c2648ff3e66519a565b00129fa737e864c36a334
yabutaka <tacky0630@gmail.com>
2019-04-08 16:46:09 -0700

    Initial Commit

---
+++ Parstagram/PostCell.swift
@@ -0,0 +1,30 @@
+//
+//  PostCell.swift
+//  Parstagram
+//
+//  Created by 薮田隆司 on 08/04/2019.
+//  Copyright © 2019 Takashi.Co. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
